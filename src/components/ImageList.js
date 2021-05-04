e64fb576baae29207156b5e985c136e8cf91f48d
Mauricio Buffon <mauricio.buffon@gmail.com>
2021-05-04 19:19:26 -0300

    v2 pics app

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,15 +1,13 @@
+import "./ImageList.css";
+import ImageCard from "./ImageCard";
 import React from "react";
 
 const ImageList = (props) => {
-  return props["images"].map((image) => {
-    return (
-      <img
-        key={image.id}
-        alt={image.alt_description}
-        src={image.urls.regular}
-      />
-    );
+  const cards = props["images"].map((image) => {
+    return <ImageCard key={image.id} image={image} />;
   });
+
+  return <div className="image-list">{cards}</div>;
 };
 
 export default ImageList;
